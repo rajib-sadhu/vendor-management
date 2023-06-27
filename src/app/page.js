feea18bd8161b4dbfee67df4b6869aed52dd26f0
rajib-sadhu <rahulrajib.sadhu@gmail.com>
2023-06-27 11:39:27 +0530

    first commit

---
+++ src/app/page.js
@@ -0,0 +1,9 @@
+import Image from 'next/image'
+
+export default function Home() {
+  return (
+    <div className='h-screen grid place-content-center'>
+      <p className='text-4xl font-semibold'>Hello Vendors World</p>
+    </div>
+  )
+}
